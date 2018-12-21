863a9c3c0b4793600277c3d92c15cc8a677a2a57
Kristján Oddsson <koddsson@gmail.com>
2018-12-21 12:03:46 +0000

    seperate express app and server

--- index.js
+++ index.js
@@ -1,68 +1,8 @@
 /* eslint-disable no-console */
 
-const aws = require('aws-sdk')
-const express = require('express')
-const multer = require('multer')
-const multerS3 = require('multer-s3')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const ExifTransformer = require('exif-be-gone')
-
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({extended: true, limit: '100mb'}))
-app.use(bodyParser.json())
-
-const s3 = new aws.S3({
-  endpoint: new aws.Endpoint(process.env.S3_ENDPOINT)
-})
-
-const upload = multer({
-  storage: multerS3({
-    s3,
-    bucket: process.env.S3_BUCKET,
-    acl: 'public-read',
-    contentType(req, file, cb) {
-      // A hack that pipes the output stream through the exif transformer before after we detect the content type
-      // of the stream but before we send it to S3
-      multerS3.AUTO_CONTENT_TYPE(req, file, function(_, mime, outputStream) {
-        cb(null, mime, outputStream.pipe(new ExifTransformer({readableObjectMode: true, writableObjectMode: true})))
-      })
-    },
-    key(request, file, cb) {
-      console.log(file)
-      cb(null, file.originalname)
-    }
-  })
-}).array('file', 1)
+const app = require('./app')
 
 const port = process.env.PORT || 3000
-
-app.post('/upload', async function(request, response) {
-  const authResponse = await fetch(process.env.AUTH_PROVIDER, {
-    headers: {
-      Accept: 'application/json',
-      Authorization: request.header('Authorization')
-    }
-  })
-
-  const json = await authResponse.json()
-  if (json.me !== process.env.HOMEPAGE) {
-    return response.status(401).send('Unauthorized')
-  }
-
-  upload(request, response, function(error) {
-    if (error) {
-      console.log(error)
-      return response.status(400).send('Not found')
-    }
-    const filename = request.files[0].originalname
-    response.header('Location', `https://${process.env.S3_BUCKET}.${process.env.S3_ENDPOINT}/${filename}`)
-    return response.status(201).send('Created')
-  })
-})
-
 app.listen(port, function() {
   console.log(`Server listening on port ${port}.`)
 })
