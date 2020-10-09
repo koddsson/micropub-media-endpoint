c31e593008c9eff2f81b7cae89fb63decc9e287c
Kristján Oddsson <koddsson@gmail.com>
2020-10-09 22:00:25 +0000

    debugging in prod

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const port = process.env.PORT || 3000
 app.listen(port, function() {
   // eslint-disable-next-line no-console
-  console.log(`Server listening on port ${port}.`)
+  console.log(`Server listening on port ${port}!`)
 })
